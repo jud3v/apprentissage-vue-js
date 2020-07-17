0372c0804101f437e7327a3e4dba07a0fa8c3dbb
jud3v <user@localhost.localdomain>
2020-07-17 20:28:27 +0200

    adding the first commit of this learning route

---
+++ src/components/ItemDetail.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Item: {{ $route.params.name }}</div>
+</template>
+
+<script>
+    export default {
+        name: "ItemDetail"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import ItemDetail from "../components/ItemDetail";
+import Contact from "../views/Contact";
 
 Vue.use(VueRouter)
 
@@ -17,10 +19,20 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact
+  },
+  {
+    path: '/item/:name',
+    component: ItemDetail
   }
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
